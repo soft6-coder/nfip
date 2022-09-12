5118e55c18f68ff0b3d31700b0fd0ee14a524112
soft6-coder <90287107+soft6-coder@users.noreply.github.com>
2022-09-12 13:31:46 +0100

    Add files via upload

---
+++ src/main/resources/public/staffs.js
@@ -0,0 +1,27 @@
+let STAFFS = [
+  {
+    id: 1,
+    type: "ministry",
+    departments: [],
+  },
+  {
+    id: 2,
+    type: "departmental",
+    departments: [
+      {id: 1, department: "fishery", unit: ["fishery unit 1", "fishery unit 2"] },
+      {id: 2, department: "livestock", unit: ["livestock unit 1", "livestockunit 2"] },
+      {id: 3, department: "finance & supply", unit: ["finance unit 1", "finance unit 2"] },
+      {id: 4, department: "produce", unit: ["produce unit 1", "produce unit 2"] },
+      {id: 5, department: "marketing", unit: ["marketing unit 1", "marketing unit 2"] },
+      {id: 6, department: "vetenary", unit: ["vetenary unit 1", "vetenary unit 2"] },
+    ],
+  },
+  {
+    id: 3,
+    type: "posted",
+    departments: [
+      {id: 1, department: "fishery", unit: ["fishery unit 1", "fishery unit 2"] },
+      {id: 2, department: "livestock", unit: ["livestock unit 1", "livestock unit 2"] },
+    ],
+  },
+];
---
+++ src/main/resources/public/states.js
@@ -0,0 +1,1 @@
+let STATES = [{id: 1, name: "abia", lga: "" }, {id: 2, name: "adamawa", lga: "" }];
